d1e7cf058aad0241e13cc122b3757ba3b0dffa00
Edward L Platt <ed@elplatt.com>
2016-04-13 16:19:38 -0400

    Add database schema.

---
+++ database/__init__.py
@@ -0,0 +1,15 @@
+import ConfigParser
+import os.path
+import sqlalchemy
+import schema
+
+config = ConfigParser.ConfigParser()
+path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+config_file = os.path.join(path, 'config')
+config.read(config_file)
+
+engine = sqlalchemy.create_engine(config.get("database", "url"), echo=False)
+
+def create_tables():
+    schema.Base.metadata.create_all(engine)
+    
---
+++ database/schema.py
@@ -0,0 +1,56 @@
+import sqlalchemy
+from sqlalchemy import (
+    Column, 
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    Table)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+article_contributor = Table(
+    'articles_contributors', Base.metadata,
+    Column('article_id', ForeignKey('articles.article_id')),
+    Column('contributor_id', ForeignKey('contributors.contributor_id'))
+)
+
+article_project = Table(
+    'articles_projects', Base.metadata,
+    Column('article_id', ForeignKey('articles.article_id')),
+    Column('project_id', ForeignKey('projects.project_id'))
+)
+
+class Article(Base):
+    __tablename__ = 'articles'
+    article_id = Column(Integer, primary_key=True)
+    article_name = Column(String(256))
+    contributors = relationship(
+        'Contributor', secondary=article_contributor, back_populates="articles"
+    )
+    projects = relationship(
+        'Project', secondary=article_project, back_populates='articles')
+    
+class Contributor(Base):
+    __tablename__ = 'contributors'
+    contributor_id = Column(Integer, primary_key=True)
+    articles = relationship(
+        'Article', secondary=article_contributor, back_populates="contributors"
+    )
+
+class Project(Base):
+    __tablename__ = 'projects'
+    project_id = Column(Integer, primary_key=True)
+    project_name = Column(String(256))
+    articles = relationship(
+        'Article', secondary=article_project, back_populates="projects"
+    )
+
+class Revision(Base):
+    __tablename__ = 'revisions'
+    revision_id = Column(Integer, primary_key=True)
+    article_id = Column(Integer, ForeignKey("articles.article_id"))
+    contributor_id = Column(Integer, ForeignKey("contributors.contributor_id"))
+    timestamp = Column(DateTime)
