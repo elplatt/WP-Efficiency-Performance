b2bbbc288328908c23dee307468e9772848d88c7
Edward L Platt <ed@elplatt.com>
2016-11-04 17:11:07 -0400

    Fix return value for min cut.

--- network/min_cut.py
+++ network/min_cut.py
@@ -16,47 +16,51 @@ def pairwise(edges_from, nodes_from=[]):
         for sink in nodes:
             if source == sink:
                 continue
-            # Deep copy the graph
-            residual = dict([(k,set(v)) for k, v in edges_from.iteritems()])
-            flow = 0
-            while 1:
-                # Find a path using breadth first search
-                visited = set()
-                parent_of = { source: source } # child->parent
-                # Deque keeps the order, set makes it fast to check for a node
-                to_visit = deque([source])
-                to_visit_s = set([source])
-                while len(to_visit) > 0:
-                    # Get a new node from the queue and mark as visited
-                    current = to_visit.popleft()
-                    to_visit_s.add(current)
-                    visited.add(current)
-                    # Get the node's neighbors and check for the sink
-                    try:
-                        neighbors = residual[current]
-                    except KeyError:
-                        # No neighbors
-                        continue
-                    if sink in neighbors:
-                        parent_of[sink] = current
-                        break
-                    # No sink, add neighbors to the queue unless we've seen them already
-                    new_neighbors = (neighbors - visited) - to_visit_s
-                    to_visit.extend(new_neighbors)
-                    to_visit_s = to_visit_s | new_neighbors
-                    # For each new neighbor, add it to the BFS tree with the
-                    # current node as parent
-                    parent_of.update(dict([(child,current) for child in new_neighbors]))
-                try:
-                    # Backtrack through the tree to construct the path
-                    child = sink
-                    while child != source:
-                        parent = parent_of[child]
-                        # Remove edge
-                        residual[parent].remove(child)
-                        child = parent
-                    flow += 1
-                except KeyError:
-                    # Sink not found in BFS tree, no more paths
-                    break
+            flow = one_pair(edges_from, source, sink)
             yield(flow)
+
+def one_pair(edges_from, source, sink):
+    # Deep copy the graph
+    residual = dict([(k,set(v)) for k, v in edges_from.iteritems()])
+    flow = 0
+    while 1:
+        # Find a path using breadth first search
+        visited = set()
+        parent_of = { source: source } # child->parent
+        # Deque keeps the order, set makes it fast to check for a node
+        to_visit = deque([source])
+        to_visit_s = set([source])
+        while len(to_visit) > 0:
+            # Get a new node from the queue and mark as visited
+            current = to_visit.popleft()
+            to_visit_s.add(current)
+            visited.add(current)
+            # Get the node's neighbors and check for the sink
+            try:
+                neighbors = residual[current]
+            except KeyError:
+                # No neighbors
+                continue
+            if sink in neighbors:
+                parent_of[sink] = current
+                break
+            # No sink, add neighbors to the queue unless we've seen them already
+            new_neighbors = (neighbors - visited) - to_visit_s
+            to_visit.extend(new_neighbors)
+            to_visit_s = to_visit_s | new_neighbors
+            # For each new neighbor, add it to the BFS tree with the
+            # current node as parent
+            parent_of.update(dict([(child,current) for child in new_neighbors]))
+        try:
+            # Backtrack through the tree to construct the path
+            child = sink
+            while child != source:
+                parent = parent_of[child]
+                # Remove edge
+                residual[parent].remove(child)
+                child = parent
+            flow += 1
+        except KeyError:
+            # Sink not found in BFS tree, no more paths
+            break
+    return flow
