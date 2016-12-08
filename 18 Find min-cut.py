2f4b899a86e019b5d94d673ed5e6c06ebf97502c
Edward L Platt <ed@elplatt.com>
2016-12-08 11:40:27 -0500

    Min cut for 10 networks.

--- 18 Find min-cut.py
+++ 18 Find min-cut.py
@@ -14,11 +14,15 @@ import network
 
 exp_name = "18_find_min_cut"
 edges_file = "archive/17_create_coeditor/2016-11-05 16:42:01 8850183/%d-coeditor.mp"
-out_file = "%d-flows.csv"
 num_proc = 12
 log_period = 30
-project_id = 23
 sample_count = 16
+to_sample = False
+if to_sample:
+    out_file = "%d-flows-sampled.csv"
+else:
+    out_file = "%d-flows.csv"
+projects_to_run = [42,543,1449,540,1162,1716,447,887,2139,280]
 
 
 # In[2]:
@@ -44,77 +48,81 @@ def run_min_cut(edges_from, pairs, done_q, return_q, log=None):
 
 exp = logbook.Experiment(exp_name)
 log = exp.get_logger()
-all_nodes = set()
-edge_count = 0
-edges_from = {}
-log.info("Loading network edges")
-with open(edges_file % project_id, "rb") as f:
-    unpacker = msgpack.Unpacker(f)
-    for o in unpacker:
-        edge_count += len(o[1])
-        edges_from[o[0][0]] = o[1]
-        all_nodes.add(o[0][0])
-        all_nodes |= set(o[1])
-log.info("  Loaded %d nodes and %d edges" % (len(all_nodes), edge_count))
-log.info("Starting %d processes" % num_proc)
-all_nodes = list(all_nodes)
-sample_pairs = network.min_cut.sample_pairs(all_nodes, sample_count)
-pair_count = len(sample_pairs)
-step = 1 + pair_count / num_proc
-return_q = Queue()
-done_q = Queue()
-workers = []
-for i in range(num_proc):
-    chunk = sample_pairs[(i*step):((i+1)*step)]
-    #core_log = exp.get_logger(name=str(i))
-    core_log = None
-    args = (edges_from, chunk, done_q, return_q, core_log)
-    p = Process(target=run_min_cut, args=args)
-    p.start()
-    workers.append(p)
-log.info("Waiting for results")
-try:
-    with open(exp.get_filename(out_file % project_id), "wb") as out:
-        out.write("source_id,sink_id,flow\n")
-        complete = 0
-        last_complete = 0
-        proc_complete = 0
-        timeout = 1 # second
-        while proc_complete < num_proc or not return_q.empty():
-            # Check for completed threads
-            if (done_q.qsize() > 0):
+for pcount, project_id in enumerate(projects_to_run):
+    all_nodes = set()
+    edge_count = 0
+    edges_from = {}
+    log.info("Loading network edges for project %d (%d/%d)" %
+             (project_id, pcount, len(projects_to_run)))
+    with open(edges_file % project_id, "rb") as f:
+        unpacker = msgpack.Unpacker(f)
+        for o in unpacker:
+            edge_count += len(o[1])
+            edges_from[o[0][0]] = o[1]
+            all_nodes.add(o[0][0])
+            all_nodes |= set(o[1])
+    log.info("  Loaded %d nodes and %d edges" % (len(all_nodes), edge_count))
+    log.info("Starting %d processes" % num_proc)
+    all_nodes = list(all_nodes)
+    if to_sample:
+        sample_pairs = network.min_cut.sample_pairs(all_nodes, sample_count)
+    else:
+        sample_pairs = list(network.min_cut.pair_iter(all_nodes))
+    pair_count = len(sample_pairs)
+    step = 1 + pair_count / num_proc
+    return_q = Queue()
+    done_q = Queue()
+    workers = []
+    for i in range(num_proc):
+        chunk = sample_pairs[(i*step):((i+1)*step)]
+        #core_log = exp.get_logger(name=str(i))
+        core_log = None
+        args = (edges_from, chunk, done_q, return_q, core_log)
+        p = Process(target=run_min_cut, args=args)
+        p.start()
+        workers.append(p)
+    log.info("Waiting for results")
+    try:
+        with open(exp.get_filename(out_file % project_id), "wb") as out:
+            out.write("source_id,sink_id,flow\n")
+            complete = 0
+            last_complete = 0
+            proc_complete = 0
+            timeout = 1 # second
+            while proc_complete < num_proc or not return_q.empty():
+                # Check for completed threads
+                if (done_q.qsize() > 0):
+                    try:
+                        done_q.get(True, timeout)
+                        proc_complete += 1
+                    except Empty:
+                        pass
                 try:
-                    done_q.get(True, timeout)
-                    proc_complete += 1
+                    flow = return_q.get(True, timeout)
+                    out.write("%d,%d,%d\n" % flow)
+                    complete += 1
                 except Empty:
                     pass
-            try:
-                flow = return_q.get(True, timeout)
-                out.write("%d,%d,%d\n" % flow)
-                complete += 1
-            except Empty:
-                pass
-            if complete % 25000 == 0 and complete != last_complete:
-                last_complete = complete
-                out.flush()
-                log.info(
-                    "  %d of %d pairs and %d of %d cores complete"
-                    % (complete, pair_count, proc_complete, num_proc))
+                if complete % 25000 == 0 and complete != last_complete:
+                    last_complete = complete
+                    out.flush()
+                    log.info(
+                        "  %d of %d pairs and %d of %d cores complete"
+                        % (complete, pair_count, proc_complete, num_proc))
+            log.info(
+                "  %d of %d pairs and %d of %d cores complete"
+                % (complete, pair_count, proc_complete, num_proc))
+        log.info("Terminating workers")
+        [p.terminate() for p in workers]
+    except KeyboardInterrupt:
+        log.info("Keyboard interrupt")
         log.info(
-            "  %d of %d pairs and %d of %d cores complete"
-            % (complete, pair_count, proc_complete, num_proc))
-    log.info("Terminating workers")
-    [p.terminate() for p in workers]
-    log.info("Done")
-except KeyboardInterrupt:
-    log.info("Keyboard interrupt")
-    log.info(
-            "  %d of %d pairs and %d of %d cores complete"
-            % (complete, pair_count, proc_complete, num_proc))
-
-    log.info("Terminating workers")
-    [p.terminate() for p in workers]
-    log.info("Done")
+                "  %d of %d pairs and %d of %d cores complete"
+                % (complete, pair_count, proc_complete, num_proc))
+    
+        log.info("Terminating workers")
+        [p.terminate() for p in workers]
+log.info("Done with all projects")
 
 
 # In[ ]:
