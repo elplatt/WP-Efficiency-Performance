33ada138e50d223aaec6b618e86bfc5ec4f2875d
Edward L Platt <ed@elplatt.com>
2017-05-11 16:12:39 -0400

    Update analysis scripts for mean editors per article.

--- analysis/b_efficiency-degree_skew.r
+++ analysis/b_efficiency-degree_skew.r
@@ -1,19 +1,19 @@
 require(dplyr)
 
-combined <- read_csv("~/WP-Efficiency-Performance/output/26_combine_data/2017-05-03 09:38:16 40d9996/combined.csv")
-vars <- c("in_degree_skew", "out_degree_skew", "N_b", "Delta_b", "article_count", "contributor_count", "first_assessment", "mean_article_age", "revision_count")
+combined <- read_csv("~/WP-Efficiency-Performance/output/26_combine_data/2017-05-11 13:59:16 f1901f4/combined.csv")
+vars <- c("in_degree_skew", "out_degree_skew", "N_b", "Delta_b", "mean_editors", "article_count", "contributor_count", "first_assessment", "mean_article_age", "revision_count")
 
 df <- combined[vars]
 df <- filter(df, N_b > 0 & Delta_b > 0)
 df$b_efficiency = df$Delta_b / df$N_b
 df$b_efficiency = df$b_efficiency / mean(df$b_efficiency)
 
-m_con = lm(log(b_efficiency) ~ log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
+m_con = lm(log(b_efficiency) ~ log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m_con)
 
-m_eff_in = lm(log(b_efficiency) ~ log(in_degree_skew) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
-summary(m_perf_in)
+m_eff_in = lm(log(b_efficiency) ~ log(in_degree_skew) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
+summary(m_eff_in)
 
-m_eff_out = lm(log(b_efficiency) ~ log(out_degree_skew) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
-summary(m_perf_out)
+m_eff_out = lm(log(b_efficiency) ~ log(out_degree_skew) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
+summary(m_eff_out)
 
--- analysis/performance-degree_skew.r
+++ analysis/performance-degree_skew.r
@@ -1,18 +1,18 @@
 require(readr)
 require(dplyr)
 
-combined <- read_csv("~/WP-Efficiency-Performance/output/26_combine_data/2017-05-03 09:38:16 40d9996/combined.csv")
-vars <- c("in_degree_skew", "out_degree_skew", "to_ga", "to_fa", "article_count", "contributor_count", "first_assessment", "mean_article_age", "revision_count")
+combined <- read_csv("~/WP-Efficiency-Performance/output/26_combine_data/2017-05-11 13:59:16 f1901f4/combined.csv")
+vars <- c("in_degree_skew", "out_degree_skew", "to_ga", "to_fa", "mean_editors", "article_count", "contributor_count", "first_assessment", "mean_article_age", "revision_count")
 
 df <- combined[vars]
 df <- filter(df, to_ga > 0 & to_fa > 0)
 df$performance = df$to_fa / (df$to_fa + df$to_ga)
 
-m_con = lm(log(performance) ~ log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
+m_con = lm(log(performance) ~ log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m_con)
 
-m_perf_in = lm(log(performance) ~ log(in_degree_skew) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
+m_perf_in = lm(log(performance) ~ log(in_degree_skew) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m_perf_in)
 
-m_perf_out = lm(log(performance) ~ log(out_degree_skew) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
+m_perf_out = lm(log(performance) ~ log(out_degree_skew) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m_perf_out)
