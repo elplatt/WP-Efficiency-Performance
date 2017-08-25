84a1ad55fa36618aa7344f27147fbb92fbb99ff9
Edward L Platt <ed@elplatt.com>
2017-08-25 17:25:56 -0400

    Update analysis.

--- analysis/complete_performance/comp_perf-all.R
+++ analysis/complete_performance/comp_perf-all.R
@@ -1,5 +1,6 @@
 require(readr)
 require(dplyr)
+require(broom)
 
 combined <- read_csv("~/WP-Efficiency-Performance/output/26_combine_data/2017-06-05 11:31:37 99c0812/combined.csv")
 vars <- c("connectivity", "in_degree_skew", "stage_N_c", "stage_Delta_c", "out_degree_skew", "to_ga", "to_fa", "path_mean", "degree_mean", "mean_editors", "article_count", "contributor_count", "first_assessment", "mean_article_age", "revision_count")
@@ -41,7 +42,15 @@ summary(m4_out)
 m5_in = lm(log(comp_perf) ~ connectivity + log(c_efficiency) + log(path_mean) + log(in_degree_skew) + log(degree_mean) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m5_in)
 
+m5_in_scale = lm(scale(log(comp_perf)) ~ scale(connectivity) + scale(log(c_efficiency)) + scale(log(path_mean)) + scale(log(in_degree_skew)) + scale(log(degree_mean)) + scale(log(mean_editors)) + scale(log(article_count)) + scale(log(contributor_count)) + scale(log(revision_count)) + scale(first_assessment) + scale(mean_article_age), data=df)
+summary(m5_in_scale)
+write.csv(tidy(m5_in_scale), "perf_in_scale.csv")
+
 m5_out = lm(log(comp_perf) ~ connectivity + log(c_efficiency) + log(path_mean) + log(out_degree_skew) + log(degree_mean) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m5_out)
+
+m5_out_scale = lm(scale(log(comp_perf)) ~ scale(connectivity) + scale(log(c_efficiency)) + scale(log(path_mean)) + scale(log(out_degree_skew)) + scale(log(degree_mean)) + scale(log(mean_editors)) + scale(log(article_count))  + scale(log(contributor_count)) + scale(log(revision_count)) + scale(first_assessment) + scale(mean_article_age), data=df)
+summary(m5_out_scale)
+write.csv(tidy(m5_out_scale), "perf_out_scale.csv")
 
 
--- analysis/stage_efficiency/stage_b_efficiency-all.R
+++ analysis/stage_efficiency/stage_b_efficiency-all.R
@@ -1,5 +1,6 @@
 require(readr)
 require(dplyr)
+require(broom)
 
 combined <- read_csv("~/WP-Efficiency-Performance/output/26_combine_data/2017-06-05 11:31:37 99c0812/combined.csv")
 vars <- c("connectivity", "degree_mean", "path_mean", "in_degree_skew", "out_degree_skew", "stage_N_b", "stage_Delta_b", "mean_editors", "article_count", "contributor_count", "first_assessment", "mean_article_age", "revision_count")
@@ -34,7 +35,14 @@ coefplot(m_perf_in,mar=c(1,6,6,1),main="Performance")
 m4_in = lm(log(a_efficiency) ~ connectivity + log(path_mean) + log(in_degree_skew) + log(degree_mean) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m4_in)
 
+m4_in_scale = lm(scale(log(a_efficiency)) ~ scale(connectivity) + scale(log(path_mean)) + scale(log(in_degree_skew)) + scale(log(degree_mean)) + scale(log(mean_editors)) + scale(log(article_count))  + scale(log(contributor_count)) + scale(log(revision_count)) + scale(first_assessment) + scale(mean_article_age), data=df)
+summary(m4_in_scale)
+write.csv(tidy(m4_in_scale), "beff_in_scale.csv")
+
 m4_out = lm(log(a_efficiency) ~ connectivity + log(path_mean) + log(out_degree_skew) + log(degree_mean) + log(mean_editors) + log(article_count)  + log(contributor_count) + log(revision_count) + first_assessment + mean_article_age, data=df)
 summary(m4_out)
 
+m4_out_scale = lm(scale(log(a_efficiency)) ~ scale(connectivity) + scale(log(path_mean)) + scale(log(out_degree_skew)) + scale(log(degree_mean)) + scale(log(mean_editors)) + scale(log(article_count))  + scale(log(contributor_count)) + scale(log(revision_count)) + scale(first_assessment) + scale(mean_article_age), data=df)
+summary(m4_out_scale)
+write.csv(tidy(m4_out_scale), "beff_out_scale.csv")
 
